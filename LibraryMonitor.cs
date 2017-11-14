13789e8755a43327a8fb423bc7a5337c1c12cf79
james <judilsteve>
2017-11-14 22:00:52 +0800

    Remove FileSystemWatcher since it wasn't working/cross platform anyway

--- LibraryMonitor.cs
+++ LibraryMonitor.cs
@@ -16,12 +16,12 @@ namespace fastmusic
     {
         private static LibraryMonitor m_instance;
 
-        private List<FileSystemWatcher> m_monitors = new List<FileSystemWatcher>();
-
         private List<String> m_libraryLocations;
         private List<String> m_fileTypes;
 
         private Timer m_syncTimer;
+
+        private const int SYNC_INTERVAL_SECONDS = 120; // TODO Make this user configurable
 
         private const int SAVE_TO_DISK_INTERVAL = 512;
 
@@ -36,28 +36,9 @@ namespace fastmusic
             m_libraryLocations = libraryLocations;
             m_fileTypes = fileTypes;
 
-            foreach(var libraryLocation in m_libraryLocations)
-            {
-                // FileSystemWatcher doesn't support watching multiple file patterns
-                // A separate watcher object must be created for each file type
-                foreach(var fileType in m_fileTypes)
-                {
-                    var libMon = new FileSystemWatcher(libraryLocation,  $"*.{fileType}");
-                    libMon.Changed += new FileSystemEventHandler(async (obj, e) => await UpdateTrack(e.FullPath));
-                    libMon.Created += new FileSystemEventHandler(async (obj, e) => await AddTrack(e.FullPath));
-                    libMon.Deleted += new FileSystemEventHandler(async (obj, e) => await RemoveTrack(e.FullPath));
-                    libMon.Renamed += new RenamedEventHandler(async (obj, e) => await UpdateTrackFileName(e.OldFullPath, e.FullPath));
-                    libMon.Renamed += new RenamedEventHandler(async (obj, e) => await UpdateTrackFileName(e.OldFullPath, e.FullPath));
-                    libMon.EnableRaisingEvents = true;
-                    m_monitors.Add(libMon);
-                }
-            }
-
-            Console.Out.WriteLine("LibraryMonitor: Registered library monitors.");
-
             // Apparently FileSystemWatcher isn't 100% reliable
             // Schedule a task to synchronise library and db every so often
-            m_syncTimer = new Timer(async (o) => await m_instance.SynchroniseDb(), null, 0, 2 * 60 * 1000);
+            m_syncTimer = new Timer(async (o) => await SynchroniseDb(), null, 0, SYNC_INTERVAL_SECONDS * 1000);
             Console.Out.WriteLine("LibraryMonitor: Set up synchronisation routine.");
         }
 
@@ -114,6 +95,7 @@ namespace fastmusic
 
         private bool DoesFileNeedUpdate(string fileName, MusicProvider mp)
         {
+            // TODO Use cached earlier LastUpdateTime to speed this up
             if(!mp.AllTracks.Any( t => t.FileName == fileName))
             {
                 // This file is not in the database at all
@@ -187,106 +169,6 @@ namespace fastmusic
                 }
             }
             await mp.SaveChangesAsync();
-        }
-        }
-
-        private async Task UpdateTrack(string trackFileName)
-        {
-        using(MusicProvider mp = new MusicProvider())
-        {
-
-            if(!(await mp.AllTracks.AnyAsync( t => t.FileName == trackFileName )))
-            {
-                await Console.Error.WriteLineAsync("LibraryMonitor: Received update event for track that was not in the database");
-                await AddTrack(trackFileName);
-            }
-
-            await Console.Out.WriteLineAsync($"LibraryMonitor: Updating: {trackFileName}.");
-
-            var trackToUpdate = await mp.AllTracks.FirstAsync( t =>
-                t.FileName == trackFileName
-            );
-
-            trackToUpdate.SetTrackData(TagLib.File.Create(trackFileName).Tag);
-
-            mp.AllTracks.Update(trackToUpdate);
-
-            await mp.SaveChangesAsync();
-
-            }
-        }
-
-        private async Task AddTrack(string trackFileName)
-        {
-            using(MusicProvider mp = new MusicProvider())
-            {
-
-            if(await mp.AllTracks.AnyAsync( t => t.FileName == trackFileName ))
-            {
-                await Console.Error.WriteLineAsync("LibraryMonitor: Received creation event for track that was already in the database");
-                await UpdateTrack(trackFileName);
-            }
-
-            await Console.Out.WriteLineAsync($"LibraryMonitor: Adding: {trackFileName}.");
-
-            var newTrack = new DbTrack();
-
-            newTrack.SetTrackData(TagLib.File.Create(trackFileName).Tag);
-
-            await mp.AllTracks.AddAsync(newTrack);
-
-            await mp.SaveChangesAsync();
-
-        }
-        }
-
-        private async Task RemoveTrack(string trackFileName)
-        {
-        using(MusicProvider mp = new MusicProvider())
-        {
-
-            if(!(await mp.AllTracks.AnyAsync( t => t.FileName == trackFileName )))
-            {
-                await Console.Error.WriteLineAsync("LibraryMonitor: Received deletion event for track that was not in the database");
-                return;
-            }
-
-            await Console.Out.WriteLineAsync($"LibraryMonitor: Removing: {trackFileName}.");
-
-            var trackToDelete = await mp.AllTracks.FirstAsync( t =>
-                t.FileName == trackFileName
-            );
-
-            mp.AllTracks.Remove(trackToDelete);
-
-            await mp.SaveChangesAsync();
-
-        }
-        }
-
-        private async Task UpdateTrackFileName(string oldFileName, string newFileName)
-        {
-        using(MusicProvider mp = new MusicProvider())
-        {
-
-            if(!(await mp.AllTracks.AnyAsync( t => t.FileName == oldFileName )))
-            {
-                await Console.Error.WriteLineAsync("LibraryMonitor: Received update filename event for track that was not in the database");
-                await AddTrack(newFileName);
-            }
-
-            await Console.Out.WriteLineAsync($"LibraryMonitor: Updating filename: \"{oldFileName}\" -> \"{newFileName}\".");
-
-            var trackToUpdate = await mp.AllTracks.FirstAsync( t =>
-                t.FileName == oldFileName
-            );
-
-            trackToUpdate.FileName = newFileName;
-
-            mp.AllTracks.Update(trackToUpdate);
-
-            await mp.SaveChangesAsync();
-
         }
         }
     }
