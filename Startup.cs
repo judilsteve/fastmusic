14fcaf7d211dcee7253b0f56795df8770ce153fd
James Udiljak <judilsteve@gmail.com>
2017-11-25 21:47:00 +0800

    The rest of the XMLDoc

--- Startup.cs
+++ Startup.cs
@@ -14,12 +14,23 @@ using fastmusic.DataProviders;
 
 namespace fastmusic
 {
+    /// <summary>
+    /// Startup class for the ASP.NET Core side of the program.
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Here to satisfy ASP.NET core requirements.
+        /// </summary>
         public Startup()
         {
         }
 
+        /// <summary>
+        /// Configures the services that will be available to the Web API classes
+        /// via the dependency injector.
+        /// </summary>
+        /// <param name="services">A collection of services to be added to.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<MusicProvider>();
@@ -29,6 +40,11 @@ namespace fastmusic
             services.AddMvc();
         }
 
+        /// <summary>
+        /// Configures the ASP.NET Core Web API.
+        /// </summary>
+        /// <param name="app">ASP.NET Core application builder, where configuration can be provided to the Web API on startup.</param>
+        /// <param name="env">Information about the Web API's hosting environment.</param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
