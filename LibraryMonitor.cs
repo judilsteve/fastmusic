9793ed926eb0c13f18ce00b6c57b2ebee89030ec
james <judilsteve>
2021-02-10 11:43:09 +0800

    WIP

--- LibraryMonitor.cs
+++ LibraryMonitor.cs
@@ -171,29 +171,110 @@ namespace fastmusic
             await musicContext.BulkInsertAsync(newArt, cancellationToken: cancellationToken);
         }
 
-        private (List<string> added, List<string> updated, List<string> unchanged) EnumerateFiles(IEnumerable<string> filePatterns, DateTime? lastDbUpdateTime, CancellationToken cancellationToken)
+        private struct FileSearch
         {
-            var added = new List<string>();
-            var updated = new List<string>();
-            var unchanged = new List<string>();
+            public readonly DateTime? LastUpdateTime;
+            public readonly string[] TrackFilePatterns;
+            public readonly string[] ArtFilePatterns;
+
+            public FileSearch(
+                DateTime? lastUpdateTime,
+                string[] trackFilePatterns,
+                string[] artFilePatterns)
+            {
+                LastUpdateTime = lastUpdateTime;
+                TrackFilePatterns = trackFilePatterns;
+                ArtFilePatterns = artFilePatterns;
+            }
+        }
+
+        private LibraryStatus EnumerateFiles(FileSearch fileSearch, CancellationToken cancellationToken)
+        {
+            var status = new LibraryStatus();
             foreach(var directory in configuration.LibraryLocations)
             {
-                foreach(var filePattern in filePatterns)
+                EnumerateFilesInner(fileSearch, directory, libraryStatus, cancellationToken);
+            }
+            return status;
+        }
+
+        private class LibraryStatus
+        {
+            public struct TrackFile
+            {
+                public readonly string TrackPath;
+                public readonly IList<string>? ArtPaths;
+
+                public TrackFile(string trackPath, IList<string>? artPaths)
                 {
-                    foreach(var filePath in Directory.EnumerateFiles(directory, filePattern, SearchOption.AllDirectories))
-                    {
-                        var fileInfo = new FileInfo(filePath);
-                        if(lastDbUpdateTime is null || fileInfo.CreationTimeUtc > lastDbUpdateTime)
-                            added.Add(filePath);
-                        else if(fileInfo.LastWriteTimeUtc > lastDbUpdateTime)
-                            updated.Add(filePath);
-                        else
-                            unchanged.Add(filePath);
-                        cancellationToken.ThrowIfCancellationRequested();
-                    }
+                    TrackPath = trackPath;
+                    ArtPaths = artPaths;
                 }
             }
-            return (added, updated, unchanged);
+
+            public class FilesStatus<T>
+            {
+                public readonly List<T> Added = new();
+                public readonly List<T> Updated = new();
+                public readonly List<T> Unchanged = new();
+            }
+
+            public FilesStatus<TrackFile> Tracks = new();
+            public FilesStatus<string> Art = new();
+        }
+
+        private void EnumerateFilesInner(
+            FileSearch search,
+            string basePath,
+            ref LibraryStatus status,
+            CancellationToken cancellationToken)
+        {
+            // TODO Write time is not enough to determine whether or not there has been a change
+            // e.g. the previous first match could have been deleted
+            // We can avoid this conundrum by capturing all art files for a track
+            // However, that still doesn't help us in the case where the file patterns change (note: this applies to tracks too)
+            // In that case, a full rebuild/diff of the db is probably required
+            var artFilePaths = new List<string>();
+            foreach(var artFilePattern in search.ArtFilePatterns)
+            {
+                foreach(var artFilePath in Directory.EnumerateFiles(basePath, artFilePattern, SearchOption.TopDirectoryOnly))
+                {
+                    artFilePaths.Add(artFilePath);
+                    var artFileInfo = new FileInfo(artFilePath);
+                    if(search.LastUpdateTime is null || artFileInfo.CreationTimeUtc > search.LastUpdateTime)
+                    {
+                        status.Art.Added.Add(artFilePath);
+                    }
+                    else if(artFileInfo.LastWriteTimeUtc > search.LastUpdateTime)
+                    {
+                        status.Art.Updated.Add(artFilePath);
+                    }
+                    else
+                        status.Art.Unchanged.Add(artFilePath);
+                    break;
+                }
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+            // Avoid piling up empty Lists
+            artFilePaths = artFilePaths.Count > 0 ? artFilePaths : null;
+
+            // TODO added/updated/unchanged correctly represents status of tracks, but not art -> track mappings
+            // The latter will need to be computed separately
+            foreach(var trackFilePattern in search.TrackFilePatterns)
+            {
+                foreach(var trackFilePath in Directory.EnumerateFiles(basePath, trackFilePattern, SearchOption.TopDirectoryOnly))
+                {
+                    var track = new LibraryStatus.TrackFile(trackFilePath, artFilePaths);
+                    var trackFileInfo = new FileInfo(trackFilePath);
+                    if(search.LastUpdateTime is null || trackFileInfo.CreationTimeUtc > search.LastUpdateTime)
+                        status.Tracks.Added.Add(track);
+                    else if(trackFileInfo.LastWriteTimeUtc > search.LastUpdateTime)
+                        status.Tracks.Updated.Add(track);
+                    else
+                        status.Tracks.Unchanged.Add(track);
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+            }
         }
 
         private IEnumerable<DbTrack> BuildUpdatedTracks(List<string> updated, PerformContext context, CancellationToken cancellationToken)
